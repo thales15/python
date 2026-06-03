a74e11079a3cf7729744d8950c2a8b4114baffb7
THALES VIEIRA CAMPOS <thales.vcampos@senacsp.edu.br>
2026-06-02 22:12:01 -0300

    funções de python e exercício de arquivos com a biblioteca OS

---
+++ funcao.py
@@ -0,0 +1,17 @@
+import os
+#print(os.getcwd()) #mostra a pasta atual
+#print(os.listdir(path="exercicio")) #Mostra o conteudo da pasta
+#os.chdir(path="exercicio") #navega para outra pasta
+#os.mkdir("pasta") # cria um diretório
+
+
+
+""" def somar_numero(n1, n2):
+    return n1 + n2
+
+x1 = int(input("Digite o número 1: "))
+x2 = int(input("Digite o número 2: "))
+
+resultado = somar_numero(x1, x2)
+
+print(f"A soma dos númeors é: {resultado}") """
